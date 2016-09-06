4a4afae8da7483f8b3b3bd1869e88363d1eef4c3
theouiteam <theouiteam@gmail.com>
2016-09-06 17:35:05 +0530

    Update and rename anu.php to index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <html>
 <head>
 <script>
@@ -22,7 +26,7 @@ function showUser(str) {
                 
             }
         };
-        xmlhttp.open("GET","getuser.php?q="+str,true);
+        xmlhttp.open("GET","getcompany.php?q="+str,true);
         xmlhttp.send();
     }
 }
@@ -43,7 +47,7 @@ function showModel(str,count) {
         xmlhttp.onreadystatechange = function() {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                
-               document.getElementById("modele").innerHTML = xmlhttp.responseText;
+               document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
                 
             }
         };
@@ -68,7 +72,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     
 } 
-$sql = "SELECT * FROM list GROUP BY company ";
+$sql = "SELECT COMPANY_NAME FROM tbl_car GROUP BY COMPANY_NAME ";
 $result = $conn->query($sql);
         if ($result->num_rows > 0) {?>   
 <form>
@@ -81,12 +85,20 @@ $result = $conn->query($sql);
        
         
         ?>  
-    <option value="<?php echo $row['company']; ?>"> <?php echo$row["company"]?></option>
-<?php }} ?> 
+    <option value="<?php echo $row['COMPANY_NAME']; ?>"> <?php echo$row["COMPANY_NAME"]?></option>
+<?php }}
+?> 
   
   </select>
 </form>
+    <?php
+    if(!isset($_SESSION["uquery"]))
+    {
+    $uquery="SELECT * FROM tbl_car WHERE CAR_ID IS NOT NULL";
+    $_SESSION["uquery"]=$uquery;
+    }
     
+    ?>
 <br>
 <div id="modele"><b>              </b></div>
  <div id="showmodel"><b>
@@ -97,9 +109,38 @@ $result = $conn->query($sql);
        
      </select>
 </form> 
-     </b></div>        
+     </b></div>
 
 
+<form>
+    
+    
+    
+    
+</form>
+<form>
+<select name="usercomfort" onchange="showModel(this.value,7)">
+    <option>Comfort</option>
+    <option value="15">10-20</option>
+     <option value="25">20-30</option>
+      <option value="35">30-40</option>
+       <option value="45">40-50</option>
+        <option value="55">50-60</option>
+         <option value="65">60-70</option>
+          <option value="75">70-80</option>
+           <option value="85">80-90</option>
+            <option value="95">90-1000</option>
+            
+</select>
+</form>
+<form>
+   All <input type="radio" name="fuel" onchange="showModel(this.value,8)">
+  petrol <input type="radio" name="fuel" onchange="showModel(this.value,8)">
+  diesel <input type="radio" name="fuel" onchange="showModel(this.value,8)">
+                
+    
+    
+</form>
 <div id="txtHint"><b>Person info will be listed here...</b></div>
 
 </body>
