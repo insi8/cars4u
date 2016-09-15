a3955e69e5bd95d61a32aeab0515c627f740197e
theouiteam <theouiteam@gmail.com>
2016-09-15 21:57:10 +0530

    Update getcompany.php

--- getcompany.php
+++ getcompany.php
@@ -1,131 +1,171 @@
-<!DOCTYPE html>
-<?php
-session_start();
-?>
-<html>
-<head>
+    <!DOCTYPE html>
+    <?php
+    session_start();
+    ?>
+    <html>
+    <head>
+
+  
     
-<style>
-table {
-    width: 100%;
-    border-collapse: collapse;
-}
+    
 
-table, td, th {
-    border: 1px solid black;
-    padding: 5px;
-}
+    <link rel="stylesheet" href="css/normalize.css">
+    <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
 
-th {text-align: left;}
-</style>
-</head>
-<body>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
 
-<?php
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-$q = $q =($_GET['q']);
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
 
+        <link rel="stylesheet" href="css/style1.css">
+          <link rel="stylesheet" href="css/style7.css">
+    <body>
+
+    <?php
+
+    $q = $q =($_GET['q']);
  $servername = "localhost";
-$username = "root";
-$fpassword = "";
-$dbname = "oui";
- 
+  $username = "u959295034_root";
+ $fpassword = "harikrishnan";
+ $dbname = "u959295034_oui";
 
-$con = new mysqli($servername, $username, $fpassword, $dbname);
 
-if (!$con) {
-    die('Could not connect: ' . mysqli_error($con));
-}
-$temp="sadf";
-mysqli_select_db($con,"ajax_demo");
-//$sql="SELECT * FROM list WHERE id = '$q'";
-//$result = mysqli_query($con,$sql);
-//while($row = mysqli_fetch_array($result)) {
-//    $q=$row['company'];
-//}
-$sql=$_SESSION["uquery"];
-$tempal="dsaf";
-$rr="COMPANY_NAME";
-if(!isset($_SESSION["COMPANY_NAME"]))
-    {
-//    $temp="company=";
-//    
-//    $temp=$temp."'$tempal'";
-//    echo $temp;
-}
- else {
-     $tempal=$_SESSION[$rr];
-     
-   $temp="AND COMPANY_NAME=";
-   if(!isset($_SESSION["CAR_NAME"]))
-     {
-       
-     }
- else {
-    
- 
-     $tempalb=$_SESSION["CAR_NAME"];
-   $temp2="AND CAR_NAME=";
-   $temp2=$temp2."'$tempalb'";
-    $sql= str_replace($temp2, "", $sql);
- }
-   $temp=$temp."'$tempal'";
-   $sql= str_replace($temp, "", $sql);
-  
-    
- }
-if($q!="Select")
-{
- $_SESSION["COMPANY_NAME"]=$q;
- 
- 
- $sql=$sql." "."AND COMPANY_NAME='$q'";
- echo $sql;
- $_SESSION["uquery"]=$sql;
-}
-//$sql="SELECT * FROM list WHERE company = '$q'";
-$result = mysqli_query($con,$sql);
+    $con = new mysqli($servername, $username, $fpassword, $dbname);
 
-echo "asdf";
-echo'</a>';
-echo "<table>
-<tr>
-
-<th>COMPANY_NAME</th>
-<th>CAR_NAME</th>
-<th>VARIANT_NAME</th>
-<th>E_S_PRIyCE</th>
-
-</tr>";
-while($row = mysqli_fetch_array($result)) {
-    echo "<tr>";
-   $id=$row['CAR_ID'];
-    echo "<td>" . $row['COMPANY_NAME'] . "</td>";
-    echo "<td>" . $row['CAR_NAME'] . "</td>";
-     echo "<td>";
-    //echo'<a href="viewdata.php?id=' . $id . '">';
-   echo "<button onclick=viewTable('$id')>";
+    if (!$con) {
+        die('Could not connect: ' . mysqli_error($con));
+    }
+    $temp="sadf";
+    mysqli_select_db($con,"ajax_demo");
    
-    echo $row['VARIANT_NAME'] ;
-   echo "</button>";
-      echo "<td>" . $row['E_S_PRICE'] . "</td>";
+    $sql=$_SESSION["uquery"];
+    $tempal="dsaf";
+    $rr="COMPANY_NAME";
+    if(!isset($_SESSION["COMPANY_NAME"]))
+        {
+   
+    }
+     else {
+         $tempal=$_SESSION[$rr];
+
+       $temp="AND COMPANY_NAME=";
+       if(!isset($_SESSION["CAR_NAME"]))
+         {
+
+         }
+     else {
+
+
+         $tempalb=$_SESSION["CAR_NAME"];
+       $temp2="AND CAR_NAME=";
+       $temp2=$temp2."'$tempalb'";
+        $sql= str_replace($temp2, "", $sql);
+     }
+       $temp=$temp."'$tempal'";
+       $sql= str_replace($temp, "", $sql);
+
+
+     }
+    if($q!="All")
+    {
+     $_SESSION["COMPANY_NAME"]=$q;
+
+
+     $sql=$sql." "."AND COMPANY_NAME='$q'";
+    // echo $sql;
+     $_SESSION["uquery"]=$sql;
+    }
+    else{
+        $tempal=$_SESSION[$rr];
+         $temp="AND COMPANY_NAME=";
+         $temp=$temp."'$tempal'";
+       $sql= str_replace($temp, "", $sql);
+        if(!isset($_SESSION["CAR_NAME"]))
+         {
+
+         }
+     else {
+
+
+         $tempalb=$_SESSION["CAR_NAME"];
+       $temp2="AND CAR_NAME=";
+       $temp2=$temp2."'$tempalb'";
+        $sql= str_replace($temp2, "", $sql);
+     }
+      $_SESSION["uquery"]=$sql;
+       
+    }
+    $try=$sql;
+   $sql=$sql." "."LIMIT 9";
+    //$sql="SELECT * FROM list WHERE company = '$q'";
+    $result = mysqli_query($con,$sql);
+    $count=0;
+  $count=0;
+   while($row = mysqli_fetch_array($result)) 
+   {
+         $id=$row['CAR_ID'];
+
+           if($count==0)
+           {
+   echo '<div class="row">';
+           }
+ echo '<div class="col-md-4">';
+  echo '<div class="card">';
+  echo ' <div class="view overlay hm-white-slight">';
+   $link =$row['CAR_IMG_URL'];
+  echo " <img src='$link' class='img-fluid' alt=''>";
+  echo '<a href="#!">';
+  echo '<div class="mask"></div>';   
+  echo '</a>';    
+  echo '  </div>'; 
+  echo '<div class="card-block">';
+  echo ' <h5 class="card-title">'.$row['COMPANY_NAME'].' '. $row['CAR_NAME'].' '.'</h5>';
+  echo '<p class="card-text"><h6>'.$row['VARIANT_NAME'].' '.'<br>'. 'â‚¹'. $row['E_S_PRICE'].'<h6></p>'; 
+  echo '<br>';
+  echo '<div class="read-more">';
   
-    echo "</tr>";
-}
-echo "</table>";
-echo "||";
-$sql="SELECT DISTINCT CAR_NAME FROM tbl_car WHERE COMPANY_NAME='$q'";
-$result = mysqli_query($con,$sql);
-echo '<select name="usero" onchange="showModel(this.value,0)"><option>Select</option>';
-while($row = mysqli_fetch_array($result)) {
-    echo "<option>".$row['CAR_NAME']."</option>";
-    
-    
-    
-}echo "</select></form>";
+    echo "<button  onclick=viewTable('$id') class='btn btn-primary'>Read more</button>";
+  echo ' </div>';
+  echo '</div>';
+  echo '</div>';
+  echo '</div>';
+ 
+ 
+  if($count=0)
+  {
+  echo '</div>';
+  $count=3;
+  }
+  $count=$count-1;
+  }
+  
+  
+ $_SESSION["more"]=$id;
+  $result = mysqli_query($con,$try);
+  $rowcount=mysqli_num_rows($result);
+ if($rowcount>9)
+  {
+  
+  
+ $m=0;
+  echo "<button id='rem' onclick=viewMore('$m') class='push_button red'>View more</button>";}
+   
+    echo "||";
+    $sql="SELECT DISTINCT CAR_NAME FROM tbl_car WHERE COMPANY_NAME='$q'";
+    $result = mysqli_query($con,$sql);
+    echo '<select name="usero" onchange="showModel(this.value,0)"><option>All</option>';
+    while($row = mysqli_fetch_array($result)) {
+        echo "<option>".$row['CAR_NAME']."</option>";
 
 
-mysqli_close($con);
-?>
-</body>
-</html>
+
+    }echo "</select></form>";
+
+
+    mysqli_close($con);
+    ?>
+    </body>
+    </html>
